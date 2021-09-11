9536ab5055cf3a605d12bdf3e0bf08b4a6ebb275
adria-arquimbau <aarquimbau@voxelgroup.net>
2021-09-11 13:08:07 +0200

    test: GetComplexAttributePagingFromSecondPageWithTwoAttributes

--- test/Palfinger.ProductManual.Tests/Infrastructure/Repositories/AttributeRepositoryShould.cs
+++ test/Palfinger.ProductManual.Tests/Infrastructure/Repositories/AttributeRepositoryShould.cs
@@ -25,7 +25,7 @@ namespace Palfinger.ProductManual.Tests.Infrastructure.Repositories
         }
 
         [Theory, AutoData]
-        public async Task GetAttributePagingFromFirstPageWithOneAttribute(string name, string description, string imageUrl)
+        public async Task GetSimpleAttributePagingFromFirstPageWithOneAttribute(string name, string description, string imageUrl)
         {
             var product = new Product(name, description, imageUrl); 
             product.SetAttributes(new List<Attribute>{new Attribute(name, description, imageUrl)});
@@ -49,6 +49,40 @@ namespace Palfinger.ProductManual.Tests.Infrastructure.Repositories
                     config
                         .Excluding(x => x.Product)
                         .Excluding(x => x.Id));
+        }
+        
+        [Fact]
+        public async Task GetComplexAttributePagingFromSecondPageWithTwoAttributes()
+        {
+            var product = new Product("productName", "productDescription", "productImageUrl"); 
+            product.SetAttributes(new List<Attribute>{new Attribute("attributeName1", "attributeDescription1", "attributeImageUrl1")});
+            product.SetAttributes(new List<Attribute>{new Attribute("attributeName2", "attributeDescription2", "attributeImageUrl2")});
+            product.SetAttributes(new List<Attribute>{new Attribute("attributeName3", "attributeDescription3", "attributeImageUrl3")});
+            product.SetAttributes(new List<Attribute>{new Attribute("attributeName4", "attributeDescription4", "attributeImageUrl4")});
+            product.SetAttributes(new List<Attribute>{new Attribute("attributeName5", "attributeDescription5", "attributeImageUrl5")});
+            product.SetAttributes(new List<Attribute>{new Attribute("attributeName6", "attributeDescription6", "attributeImageUrl6")});
+            await _testContext.TestDbContext().Product.AddAsync(product);
+            await _testContext.TestDbContext().SaveChangesAsync();  
+                
+            var expectedResponse = new PagedList<Attribute>(new List<Attribute>
+            {
+                new Attribute("attributeName3", "attributeDescription3", "attributeImageUrl3"),
+                new Attribute("attributeName4", "attributeDescription4", "attributeImageUrl4")
+            }, 6,2,2);
+            
+
+            var request = new ManualByProductIdFilterRequest    
+            {   
+                PageNumber = 2,
+                PageSize = 2,
+                ProductId = 1
+            };
+            var response = await _repositoryWrapper.AttributeRepository.GetAttributesPaging(request);
+
+            response.Should().BeEquivalentTo(expectedResponse, config => 
+                config
+                    .Excluding(x => x.Product)
+                    .Excluding(x => x.Id));
         }
     }
 }
